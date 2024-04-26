3e75ded229708355cc95bfb860a0693bdbf3c474
Anushka <Anushkabhat9@gmail.com>
2024-04-26 16:13:01 -0400

    Add more customer details

--- transactx/Backend/Api/customer.js
+++ transactx/Backend/Api/customer.js
@@ -10,8 +10,8 @@ router.get('/customer', async (req, res) => {
     const json_body = JSON.stringify(req.body);
 
     // Execute the stored procedure with the JSON body as a parameter
-   const results = await sequelize.query('SELECT * FROM customer');
-   console.log("Results:",results)
+   const results = await sequelize.query('SELECT c.ssn,c.AccountNumber,c.username,firstname,Lastname,streetaddress,city,state,age,gender,monthly_income FROM customer c  inner join person p on c.ssn=p.ssn');
+
 
   res.status(200).json({ message: 'Customer results return successfully ' ,results:results});
   } catch (error) {
