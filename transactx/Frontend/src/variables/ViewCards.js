e198a940f6c717ec9ae66def7146da94d5d21ffd
paulie <pktpaulie@gmail.com>
2024-04-26 18:40:44 -0400

    Add Customer Details

---
+++ transactx/Frontend/src/variables/ViewCards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ViewCards = () => {
+
+    const [cards, setCards] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [cardsPerPage, setCardsPerPage] = useState(5);
+
+
+    
+
+  return (
+    <div>ViewCards</div>
+  )
+}
+
+export default ViewCards
